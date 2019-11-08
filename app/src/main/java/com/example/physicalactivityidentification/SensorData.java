0016142221b7f26a57d1eb5625aa5aa111f68ce3
Vitali  Baranov <vb1026@wildcats.unh.edu>
2019-11-07 23:14:16 -0500

    Display accel data on screen, use accel values to determine if moving

--- app/src/main/java/com/example/physicalactivityidentification/SensorData.java
+++ app/src/main/java/com/example/physicalactivityidentification/SensorData.java
@@ -7,32 +7,39 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.widget.TextView;
 
 public class SensorData implements SensorEventListener {
 
-    Activity activity;
+    private Activity activity;
     private SensorManager sensorManager;
-    Sensor accel;
+    private Sensor accel;
+    TextView xValue, yValue, zValue, action;
 
     public SensorData(Activity activity)
     {
         this.activity = activity;
         Log.d("Now", "Initializing sensors");
-
+        xValue = (TextView)this.activity.findViewById(R.id.xValue);
+        yValue = (TextView)this.activity.findViewById(R.id.yValue);
+        zValue = (TextView)this.activity.findViewById(R.id.zValue);
+        action = (TextView)this.activity.findViewById(R.id.action);
         sensorManager = (SensorManager) this.activity.getSystemService(Context.SENSOR_SERVICE);
         accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(SensorData.this,accel,sensorManager.SENSOR_DELAY_NORMAL);
         Log.d("Now","Accel has been initialized");
     }
 
-    public int getAcceleration()
-    {
-        return 0;
-    }
 
-    public String getAction()
+    private void setAction(SensorEvent event)
     {
-        return "";
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        if(x < 2.5 && x > -2 && y < 6 && y > -3 && z<11 && z > 6)
+            action.setText("Not moving");
+        else
+            action.setText("Moving");
     }
 
     @Override
@@ -44,5 +51,11 @@ public class SensorData implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         Log.d("Main:", "onSensorChanged: X:" + event.values[0] + " Y: " + event.values[1] +
                 " Z: " + event.values[2]);
+
+        xValue.setText("xValue: " + event.values[0]);
+        yValue.setText("yValue: " + event.values[1]);
+        zValue.setText("zValue: " + event.values[2]);
+        setAction(event);
     }
+
 }
